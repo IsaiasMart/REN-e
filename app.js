ab2a98b95ee8f9c54105b4d2feda36344af720e3
IsaiasMart <isaiasmcald@gmail.com>
2024-12-26 16:54:07 +0100

    mensaje5

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
-const targetElements = document.getElementsByClassName("animated-text");  
-const musicElement= document.getElementById("musica");
-const length = 4;
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            const targetElements = document.getElementsByClassName("animated-text");  
+            const musicElement= document.getElementById("musica");
+            const audio= new Audio("musica op1.mp4");
+            const length = 4;
+            const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-function generateRandomString(len) {
-    let result = "";
-    for (let i = 0; i < len; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters[randomIndex];
-    }
-    return result;
-}
+            function generateRandomString(len) {
+                let result = "";
+                for (let i = 0; i < len; i++) {
+                    const randomIndex = Math.floor(Math.random() * characters.length);
+                    result += characters[randomIndex];
+                }
+                return result;
+            }
 
-function updateText() {
-    // Itera sobre todos los elementos y actualiza su contenido
-    for (let i = 0; i < targetElements.length; i++) {
-        targetElements[i].textContent = generateRandomString(length);
-    }
-}
-musicElement.addEventListener("click",function(event){
-
-    
-}
-
-);
+            function updateText() {
+                
+                for (let i = 0; i < targetElements.length; i++) {
+                    targetElements[i].textContent = generateRandomString(length);
+                }
+            }
 
 
 
 
-setInterval(updateText, 600);
 
-updateText();
+            musicElement.addEventListener("click",function(event){
+                
+
+                    if (audio.paused) {
+                        audio.play(); // Reproduce la canción si está pausada
+                        musicElement.textContent = "Pausar Canción"; // Cambia el texto del botón
+                      } else {
+                        audio.pause();
+                        musicElement.textContent = "Reproducir Canción"; // Cambia el texto del botón
+                      }
+                    }
+                
+            );
+
+
+
+
+            setInterval(updateText, 600);
+
+            updateText();
